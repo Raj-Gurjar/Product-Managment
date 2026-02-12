1225dd51f36e8ecf8b116a1cdb0a1424e48e474f
Raj <rajchhalotre14@gmail.com>
2026-02-12 21:46:39 +0530

    Dockerized the project

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,9 +5,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  app.setGlobalPrefix('api');
+  
   // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:4173'],
+    origin: ['http://localhost', 'http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://localhost:4173'],
     methods: ['GET', 'POST', 'PATCH', 'DELETE'],
     credentials: true,
   });
