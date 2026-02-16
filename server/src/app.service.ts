ef7ed9214fc93c9e336033ca47f4c7c8edff682c
Raj <rajchhalotre14@gmail.com>
2026-02-16 12:55:18 +0530

    updated docker config

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -4,5 +4,6 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   getHello(): string {
     return `Server is Runnig in ${process.env.NODE_ENV} on port ${process.env.PORT}`;
-  }
+  } 
 }
+ 
